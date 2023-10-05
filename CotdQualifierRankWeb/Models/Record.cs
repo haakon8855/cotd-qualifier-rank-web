fa971b31f99aea0410ae57f28d72b1b3d61f04fa
Håkon Anders Strømsodd <haakon8855@gmail.com>
2023-10-05 15:50:59 +0200

    Add string formatting for record

--- CotdQualifierRankWeb/Models/Record.cs
+++ CotdQualifierRankWeb/Models/Record.cs
@@ -4,5 +4,18 @@
     {
         public int Id { get; set; }
         public int Time { get; set; }
+
+        public override string ToString()
+        {
+            // Return string with time in format "ss.ttt" or "mm:ss.ttt" (if necessary):
+            if (Time < 60000)
+            {
+                return $"{Time / 1000}.{Time % 1000:000}";
+            }
+            else
+            {
+                return $"{Time / 60000}:{Time / 1000 % 60:00}.{Time % 1000:000}";
+            }
+        }
     }
 }
