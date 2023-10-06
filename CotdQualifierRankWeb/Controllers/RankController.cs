ff86532f4ae91168631fdd4fa3118f8e605f4029
Håkon Anders Strømsodd <haakon8855@gmail.com>
2023-10-06 09:49:13 +0200

    Add qualifier rank dummy endpoint

---
+++ CotdQualifierRankWeb/Controllers/RankController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CotdQualifierRankWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{mapUID}/{time:int}")]
+        public IActionResult GetAction(string mapUID, int time)
+        {
+            return Ok(new { pb = mapUID, rank = time });
+        }
+    }
+}
